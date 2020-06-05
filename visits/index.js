8c674f0a45ed3a4f08ac9adc0a76faba30fbd169
Jhon Zuñiga <zuni230669>
2020-06-04 21:32:49 -0500

    docker-compose examples

--- visits/index.js
+++ visits/index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const redis = require('redis');
 const app = express();
 const client = redis.createClient({
-    host: 'redis-server',
+    host: 'localhost',
+    //host: 'redis-server',
     port: 6379
 });
-client.set('visits', 0);
+
 app.get('/', (req, res) => {
     
     client.get('visits', (err, visits) => {
+        if (isNaN(parseInt(visits))) {
+            client.set('visits', 0);
+            visits = 0;
+        }            
         visits = parseInt(visits) + 1;
         client.set('visits', visits);
         res.send('Number of visits is ' + visits);        
